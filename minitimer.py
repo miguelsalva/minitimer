306cb7b4e16dec070427d273651e1975e533db6e
miguelsalva <miguelsalva@gmail.com>
2019-11-27 20:27:22 +0100

    Add format with two digits for time

--- minitimer.py
+++ minitimer.py
@@ -3,15 +3,24 @@
 import sys
 import time
 
-
-DEFAULT_TIME = 15  # 25min by default for Pomodoro Technique
+DEFAULT_TIME = 1500  # 25min by default for Pomodoro Technique
 
 
 def format_time(s):
     """Function that gets a time in seconds and returns it in format MM:SS"""
     minutes = int(s / 60)
     seconds = s % 60
-    print(str(minutes) + ":" + str(seconds), end="\r")
+    if minutes < 10 and seconds < 10:
+        print("0"+ str(minutes) + ":" + "0" + str(seconds), end="\r")
+    else:
+        if minutes >= 10 and seconds < 10:
+            print(str(minutes) + ":" + "0" + str(seconds), end="\r")
+        else:
+            if minutes < 10 and seconds >= 10:
+                print("0" + str(minutes) + ":" + str(seconds), end="\r")
+            else:
+                print(str(minutes) + ":" + str(seconds), end="\r")
+
 
 t = DEFAULT_TIME
 while t > 0:
