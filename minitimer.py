d155887ee4f19c545d8f63b555e9c1f5483f2fe9
miguelsalva <miguelsalva@gmail.com>
2019-11-27 20:17:07 +0100

    Add time overwriting on the same line

--- minitimer.py
+++ minitimer.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
+import sys
 import time
 
-DEFAULT_TIME = 1500  # 25min by default for Pomodoro Technique
+
+DEFAULT_TIME = 15  # 25min by default for Pomodoro Technique
 
 
 def format_time(s):
     """Function that gets a time in seconds and returns it in format MM:SS"""
     minutes = int(s / 60)
     seconds = s % 60
-    print(str(minutes) + ":" + str(seconds))
-
+    print(str(minutes) + ":" + str(seconds), end="\r")
 
 t = DEFAULT_TIME
 while t > 0:
-    print("Remaining time:" )
+    print("Remaining time: ", end = "")
     format_time(t)
     t = t - 1
     time.sleep(1)
